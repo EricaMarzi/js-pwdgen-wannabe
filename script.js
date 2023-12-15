38db7480355947259b70bd05b08012ed5b73525b
EricaMarzi <erica.marzi.1993@gmail.com>
2023-12-15 14:50:11 +0100

    descrizione passaggi

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 console.log("js ok")
+
+
+/* 
+1- Aggiungere in HTML il tag con l'id da richiamare
+2- Richiamare l'id 
+3- Dichiare la variabile "message" e assegnarle il valore "La tua password è:"
+4- Dichiarare la variabile "number" e assegnarle il valore "23" (numero o stringa?)
+5- Chiedere all'utente il nome (prompt)
+6- Dichiarare la variabile "firstname" e assegnarle il valore in risposta al promt
+7- Chiedere all'utente il cognome (prompt)
+8- Dichiarare la variabile "lastname" e assegnarle il valore in risposta al promt
+9- Chiedere all'utente il colore preferito (prompt)
+10- Dichiarare la variabile "color" e assegnarle il valore in risposta al promt
+11- Dichiarare la variabile "password" e assegnarle il valore "nome+cognome+colore+number"
+12- Unire "message" e "password" (mettendo il grassetto alla password)
+*/ 
